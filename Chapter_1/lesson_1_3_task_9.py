6ca93522ab5a5fb7860c73d319f888f804bc6d9d
arnati <avyfa11@gmail.com>
2022-06-01 17:53:37 +0300

    Added file lesson_1_3_task_9.py

---
+++ Chapter_1/lesson_1_3_task_9.py
@@ -0,0 +1,22 @@
+import sys
+
+# программу не менять, только добавить два метода
+lst_in = list(map(str.strip, sys.stdin.readlines()))  # считывание списка строк из входного потока
+
+
+class DataBase:
+    lst_data = []
+    FIELDS = ('id', 'name', 'old', 'salary')
+
+    def insert(self, data):
+        for i in data:
+            id, name, old, salary = i.split()
+            self.lst_data.append(dict({'id': id, 'name': name, 'old': old, 'salary': salary}))
+
+    def select(self, a, b):
+        return self.lst_data[a:b + 1]
+
+
+db = DataBase()
+db.insert(lst_in)
+# print(db.select(0, 2))
