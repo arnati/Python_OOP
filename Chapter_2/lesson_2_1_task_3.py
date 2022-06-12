e7dc058a448199ebb2c6c11084af757e8866a408
arnati <avyfa11@gmail.com>
2022-06-12 14:52:04 +0300

    Added file lesson_2_1_task_3.py

---
+++ Chapter_2/lesson_2_1_task_3.py
@@ -0,0 +1,21 @@
+class Clock:
+    def __init__(self):
+        self.__time = 0
+
+    def set_time(self, tm):
+        if self.__check_time(tm):
+            self.__time = tm
+
+    def get_time(self):
+        return self.__time
+
+    @staticmethod
+    def __check_time(tm):
+        if type(tm) == int and (0 <= tm < 100000):
+            return True
+        else:
+            return False
+
+
+clock = Clock()
+clock.set_time(4530)
