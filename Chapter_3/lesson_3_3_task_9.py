b8479701b7bb41b2be022cf96ec733291a775129
arnati <avyfa11@gmail.com>
2022-06-27 19:45:49 +0300

    Added file lesson_3_3_task_9.py

---
+++ Chapter_3/lesson_3_3_task_9.py
@@ -0,0 +1,33 @@
+class Ingredient:
+    def __init__(self, name, volume, measure):
+        self.name = name
+        self.volume = volume
+        self.measure = measure
+
+    def __str__(self):
+        return f"{self.name}: {self.volume}, {self.measure}"
+
+
+class Recipe:
+    def __init__(self, *args):
+        self.lst = list(args)
+
+    def add_ingredient(self, ing):
+        self.lst.append(ing)
+
+    def remove_ingredient(self, ing):
+        self.lst.remove(ing)
+
+    def get_ingredients(self):
+        return tuple(self.lst)
+
+    def __len__(self):
+        return len(self.lst)
+
+
+recipe = Recipe()
+recipe.add_ingredient(Ingredient("Соль", 1, "столовая ложка"))
+recipe.add_ingredient(Ingredient("Мука", 1, "кг"))
+recipe.add_ingredient(Ingredient("Мясо баранины", 10, "кг"))
+ings = recipe.get_ingredients()
+n = len(recipe)  # n = 3
