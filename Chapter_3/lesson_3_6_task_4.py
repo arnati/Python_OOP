c31bcea9fc2fc66ce7dc23fb73be32657b5bce54
arnati <avyfa11@gmail.com>
2022-07-06 12:33:36 +0300

    Added file lesson_3_5_task_4.py

---
+++ Chapter_3/lesson_3_6_task_4.py
@@ -0,0 +1,19 @@
+class Rect:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    def __eq__(self, other):
+        return True if self.width == other.width and self.height == other.height else False
+
+    def __hash__(self):
+        return hash((self.height, self.width))
+
+
+r1 = Rect(10, 5, 100, 50)
+r2 = Rect(-10, 4, 100, 50)
+
+h1, h2 = hash(r1), hash(r2)   # h1 == h2
+pass
