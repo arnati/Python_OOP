7fa2d21e76e6e775c71965be0234bbf290e8a79b
arnati <avyfa11@gmail.com>
2022-07-07 18:13:18 +0300

    Added file lesson_3_7_task_5.py

---
+++ Chapter_3/lesson_3_7_task_5.py
@@ -0,0 +1,32 @@
+import sys
+
+
+class MailBox:
+    def __init__(self):
+        self.inbox_list = list()
+
+    def receive(self):
+        lst_in = list(map(str.strip, sys.stdin.readlines()))
+        self.inbox_list = [MailItem(*row.split("; ")) for row in lst_in]
+
+
+class MailItem:
+    def __init__(self, mail_from, title, content):
+        self.mail_from = mail_from
+        self.title = title
+        self.content = content
+        self.is_read = False
+
+    def set_read(self, fl_read):
+        self.is_read = fl_read
+
+    def __bool__(self):
+        return self.is_read
+
+
+mail = MailBox()
+mail.receive()
+mail.inbox_list[0].set_read(True)
+mail.inbox_list[-1].set_read(True)
+
+inbox_list_filtered = [*filter(bool, mail.inbox_list)]
