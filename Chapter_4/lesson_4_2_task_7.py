6169402ba5dff2335809404ce82a7313d921a163
arnati <avyfa11@gmail.com>
2022-07-17 17:27:39 +0300

    Added file lesson_4_2_task_7.py

---
+++ Chapter_4/lesson_4_2_task_7.py
@@ -0,0 +1,34 @@
+class VideoItem:
+    def __init__(self, title, descr, path):
+        self.title = title
+        self.descr = descr
+        self.path = path
+        self.rating = VideoRating()
+
+
+class VideoRating:
+    def __init__(self):
+        self.__rating = 0
+
+    @property
+    def rating(self):
+        return self.__rating
+
+    @rating.setter
+    def rating(self, rating):
+        self.__rating = rating
+
+    def __setattr__(self, key, value):
+        if key == "rating":
+            if type(value) is not int or not (0 <= value <= 5):
+                raise ValueError('неверное присваиваемое значение')
+        object.__setattr__(self, key, value)
+
+
+v = VideoItem('Курс по Python ООП', 'Подробный курс по Python ООР', 'D:/videos/python_oop.mp4')
+print(v.rating.rating)  # 0
+v.rating.rating = 5
+print(v.rating.rating)  # 5
+title = v.title
+descr = v.descr
+v.rating.rating = 6  # ValueError
