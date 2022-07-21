8df3d0a5df1c511c58b6bef207df8b4ef0ef035a
arnati <avyfa11@gmail.com>
2022-07-21 16:53:02 +0300

    Added file lesson_4_5_task_5.py

---
+++ Chapter_4/lesson_4_5_task_5.py
@@ -0,0 +1,25 @@
+class Validator:
+    def _is_valid(self, data):
+        raise NotImplementedError('в классе не переопределен метод _is_valid')
+
+
+class FloatValidator(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def _is_valid(self, data):
+        if type(data) is float and self.min_value <= data <= self.max_value:
+            return True
+        return False
+
+    def __call__(self, value, *args, **kwargs):
+        return self._is_valid(value)
+
+
+float_validator = FloatValidator(0, 10.5)
+res_1 = float_validator(1)  # False (целое число, а не вещественное)
+res_2 = float_validator(1.0)  # True
+res_3 = float_validator(-1.0)  # False (выход за диапазон [0; 10.5])
+
+pass
