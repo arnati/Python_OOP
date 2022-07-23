03887b7ba67dc33d472537164e8ab27988e0de2b
arnati <avyfa11@gmail.com>
2022-07-23 14:19:04 +0300

    Added file lesson_4_6_task_9.py

---
+++ Chapter_4/lesson_4_6_task_9.py
@@ -0,0 +1,56 @@
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+
+    def __sub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money - other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money - other.money)
+
+
+class Money:
+    def __init__(self, value):
+        self.check_value(value)
+        self._money = value
+
+    @staticmethod
+    def check_value(value):
+        if type(value) not in (int, float):
+            raise TypeError('сумма должна быть числом')
+
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, value):
+        self.check_value(value)
+        self._money = value
+
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"
+
+
+m1 = MoneyR(1)
+m2 = MoneyD(2)
+m = m1 - 10
+print(m)  # MoneyR: 11
+# m = m1 - 5.4
+# m = m1 + m2  # TypeError
