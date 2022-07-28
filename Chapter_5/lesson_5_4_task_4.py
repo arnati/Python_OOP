edfd893cb9c489a93828ca81bdc172d516967e64
arnati <avyfa11@gmail.com>
2022-07-28 13:54:04 +0300

    Added file lesson_5_4_task_4.py

---
+++ Chapter_5/lesson_5_4_task_4.py
@@ -0,0 +1,20 @@
+class StringException(Exception):
+    pass
+
+
+class NegativeLengthString(StringException):
+    pass
+
+
+class ExceedLengthString(StringException):
+    pass
+
+
+try:
+    raise ExceedLengthString
+except NegativeLengthString:
+    print("NegativeLengthString")
+except ExceedLengthString:
+    print("ExceedLengthString")
+except StringException:
+    print("StringException")
